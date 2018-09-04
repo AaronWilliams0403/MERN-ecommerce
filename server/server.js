4e6110decf0a03d5b0a912508773c2cc966aecbf
Vy Nguyen <nguyenhieutrieuvy@gmail.com>
2018-09-05 00:33:51 +0700

    fix

--- server/server.js
+++ server/server.js
@@ -31,7 +31,7 @@ if (process.env.NODE_ENV === "production") {
   app.use(express.static("../client/build"));
 
   const path = require("path");
-  app.get("/*", (req, res) => {
+  app.get("*", (req, res) => {
     res.sendFile(path.resolve(__dirname, "../client", "build", "index.html"));
   });
 }
