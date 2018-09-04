cb58a953769e93592b214bd3c68b2c6a40e32052
Vy Nguyen <nguyenhieutrieuvy@gmail.com>
2018-09-05 00:37:58 +0700

    fix

--- server/server.js
+++ server/server.js
@@ -28,11 +28,11 @@ require("./routes/authRoutes")(app);
 require("./routes/userRoutes")(app);
 
 if (process.env.NODE_ENV === "production") {
-  app.use(express.static("client/build"));
+  app.use(express.static("../client/build"));
 
   const path = require("path");
   app.get("*", (req, res) => {
-    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+    res.sendFile(path.resolve(__dirname, "../client", "build", "index.html"));
   });
 }
 
